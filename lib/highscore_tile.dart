480f7cc8346991de98c9a29e4f0042a949c60b79
Adityakachare <78196873+Adityakachare@users.noreply.github.com>
2024-03-19 14:45:50 +0530

    Update highscore_tile.dart

--- lib/highscore_tile.dart
+++ lib/highscore_tile.dart
@@ -3,37 +3,106 @@ import 'package:flutter/material.dart';
 
 class HighscoreTile extends StatelessWidget {
   final String documentId;
+  final int position;
   const HighscoreTile({
     Key? key,
     required this.documentId,
+    required this.position,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    //get the collection of highscores
+    // Get the collection of highscores
     CollectionReference highscores =
         FirebaseFirestore.instance.collection('highscores');
 
-    return FutureBuilder<DocumentSnapshot>(
-      future: highscores.doc(documentId).get(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          Map<String, dynamic> data =
-              snapshot.data!.data() as Map<String, dynamic>;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Display headings for the first record only
+        if (position == 1)
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'POSITION',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'NAME',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'SCORE',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        // Highscore row
+        FutureBuilder<DocumentSnapshot>(
+          future: highscores.doc(documentId).get(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              Map<String, dynamic> data =
+                  snapshot.data!.data() as Map<String, dynamic>;
 
-          return Row(
-            children: [
-              Text(data['score'].toString()),
-              SizedBox(
-                width: 10,
-              ),
-              Text(data['name']),
-            ],
-          );
-        } else {
-          return Text('Loading...');
-        }
-      },
+              // Function to get the position label based on the position
+              String getPositionLabel(int position) {
+                switch (position) {
+                  case 1:
+                    return '1st';
+                  case 2:
+                    return '2nd';
+                  case 3:
+                    return '3rd';
+                  case 4:
+                    return '4th';
+                  case 5:
+                    return '5th';
+                  default:
+                    return position.toString() + 'th';
+                }
+              }
+
+              return Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      getPositionLabel(position),
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: 10),
+                    Text(data['name']),
+                    SizedBox(width: 10),
+                    Text(data['score'].toString()),
+                  ],
+                ),
+              );
+            } else {
+              return Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Loading...'),
+                    SizedBox(width: 10),
+                    Text(''),
+                    SizedBox(width: 10),
+                    Text(''),
+                  ],
+                ),
+              );
+            }
+          },
+        ),
+      ],
     );
   }
 }
